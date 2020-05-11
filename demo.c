0ce22aa498bc2ddfeab241d4abc5bf69adb20223
ZJiawen <1654669931@qq.com>
2020-05-11 20:46:44 +0800

    Week3-2

--- demo.c
+++ demo.c
@@ -3,32 +3,11 @@
 int main(){
 	int n;
 	scanf("%d",&n);
-	int a,b,c,d;
-	d = n%10;
-	c = (n/10)%10;
-	b = (n/100)%10;
-	a = n/1000;
-	//printf("%d %d %d %d\n",a,b,c,d);
-	int m,k;
-	m = 10*a+b;
-	k = 10*c+d;
-	if(m>=8){
-		m-=8;
-	}else{
-		m=(24+m-8);
-	}
-	if(m==0){
-		if(k==0){
-			printf("%d",0);
-		}else{
-			printf("%d",k);
-		}
-	}else{
-		if(k<10){
-			printf("%d0%d",m,k);
-		}else{
-			printf("%d%d",m,k);
-		}
-	}
+	int flag=1;
+	do{
+		printf("%d ",flag);
+		flag+=2;
+	}while(flag<=n);
+	printf("\n");
 	return 0;
-}
+} 
